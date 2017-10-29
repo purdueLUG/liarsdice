dbeadddef3c8649260deaae63f220512f0da2416
Jack Cottom <iacobus96@gmail.com>
2017-10-28 23:48:45 -0400

    fix some game logic, begin implementing PlayerList class

--- server.py
+++ server.py
@@ -7,6 +7,7 @@ from autobahn.wamp.exception import ApplicationError
 from random import randint
 from itertools import cycle
 
+
 class Player:
     stash = []
     player_id = ''
@@ -26,9 +27,36 @@ class Player:
             return False
         return True
 
+class PlayerList(list):
+    def __init__(self):
+        self = []
+
+    def roll(self):
+        for p in self:
+            p.roll()
+
+    def rotate(self, n):
+        '''Rotate list l to the left n places.'''
+        self = self[n:] + self[:n]
+
+    def delete(self, player):
+        if player in self:
+            self.remove(player)
+
+    def penalize(self, player):
+        if player.stash_size > 0:
+            player.lose()
+        if player.stash_size <= 0:
+            self.delete(player)
+
+    def get_pool(self):
+        pool = []
+        for p in self:
+            pool += p.stash
+        return pool
 
 class AppSession(ApplicationSession):
-    players  = []
+    players  = PlayerList()
     prev_bet = {'num_dice': 0, 'value': 0}
     current_player = Player('', 0)
     next_player = Player('', 0)
@@ -72,13 +100,12 @@ class AppSession(ApplicationSession):
 
 
         # setup phase
-        print("Press Enter to end registration.")
+        print("Ten seconds to register...")
         yield sleep(10)
 
         while len(self.players) > 1:
             # roll all dice
-            for p in self.players:
-                p.roll()
+            self.players.roll()
 
             # publish the game board
             yield self.publish_gameboard()
@@ -93,30 +120,33 @@ class AppSession(ApplicationSession):
                 # ask for bet:
                 try:
                     new_bet = yield self.call(self.current_player.player_id+'.bet',
-                                              self.current_player.stash, self.assemble_gameboard())
+                                              self.current_player.stash,
+                                              self.assemble_gameboard())
                     print("bet() called on {} with result: {}".format(self.current_player, new_bet))
-                    if new_bet['num_dice'] > self.prev_bet['num_dice']:
+                    valid_nums = isinstance(new_bet['num_dice'], int) and isinstance(new_bet['value'], int)
+                    if valid_nums and new_bet['num_dice'] > self.prev_bet['num_dice']:
                         valid_bet = True
+                        self.prev_bet = new_bet
                     else:
+                        print("bet is invalid!")
                         valid_bet = False
-                        self.current_player.lose()
-                        if self.current_player.stash_size <= 0:
-                            self.players.remove(self.current_player)
-                    self.prev_bet = new_bet
+                        self.players.penalize(self.current_player)
                 except ApplicationError as e:
                     valid_bet = False
-                    self.players.remove(self.current_player)
+                    self.players.delete(self.current_player)
 
                 if valid_bet:
-
                     # ask next player for challenge
+                    next_ind = (ind+1)%len(self.players)
                     self.next_player = self.players[(ind+1)%len(self.players)]
                     try:
                         challenge_response = yield self.call(self.next_player.player_id+'.challenge',
-                                                             self.next_player.stash, self.assemble_gameboard())
-                        print("challenge() called on {} with result: {}".format(self.next_player, challenge_response))
+                                                             self.next_player.stash,
+                                                             self.assemble_gameboard())
+                        print("challenge() called on {} with result: {}".format(self.next_player,
+                                                                                challenge_response))
                     except ApplicationError as e:
-                        self.players.remove(self.next_player)
+                        self.players.delete(self.next_player)
 
                 yield sleep(1)
                 yield self.publish_gameboard()
@@ -127,24 +157,41 @@ class AppSession(ApplicationSession):
             # check the bet against the pool
             bet_value = self.prev_bet['value']
             bet_num_dice = self.prev_bet['num_dice']
-            num_of_value = 0
-            for p in self.players:
-                num_of_value += p.stash.count(num_of_value)
+            pool = self.players.get_pool()
+            num_of_value = pool.count(bet_value)
+
+            print("There were {} {}s".format(num_of_value, bet_value))
 
             # if the bet was good
-            if bet_value >= num_of_value:
+            if bet_num_dice <= num_of_value:
+                print("Better wins!")
+                # winner begins next round
+                self.players.rotate(ind)
+
+                # penalize loser
                 self.next_player.lose()
                 if self.next_player.stash_size <= 0:
-                    self.players.remove(self.next_player)
+                    self.players.delete(self.next_player)
             else:
+                print("Challenger wins!")
+                # winner begins next round
+                self.players.rotate(next_ind)
+
+                # penalize loser
                 self.current_player.lose()
                 if self.current_player.stash_size <= 0:
-                    self.players.remove(self.current_player)
+                    self.players.delete(self.current_player)
 
+            # need to reset this for next round
+            self.prev_bet = {'num_dice': 0, 'value': 0}
 
             yield sleep(1)
 
-        print("GAME OVER")
+        if len(self.players) >= 0:
+            print("GAME OVER, no players entered :(")
+        else:
+            print("GAME OVER, winner: " + self.players[0].player_id)
+
 
 from autobahn.twisted.wamp import ApplicationRunner
 if __name__ == '__main__':
